b1a9528646f899b6da33c5ccfb8b02d6ab45ee2c
colour-me-bibi <bennyforeman1@gmail.com>
2022-08-03 18:07:30 -0400

    Install djangorestframework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    # Third-party apps    
+    "rest_framework",
+    
+    # Local apps
+    "core",
 ]
 
 MIDDLEWARE = [
